3f92c15490ed259f6df517564534543f57ba2a67
AndrewR3K <andrew.martin.louis@gmail.com>
2022-09-03 14:29:36 -0700

    final code

--- client/client.lua
+++ client/client.lua
@@ -1,10 +1,28 @@
-RegisterCommand('open', function()
+RegisterCommand('nuiopen', function()
     SendNUIMessage({
         type = 'open'
-    })  
+    })
+    SetNuiFocus(true, true)
+end)
+
+RegisterCommand('nuiclose', function()
+    SendNUIMessage({
+        type = 'close'
+    })
+    SetNuiFocus(false, false)
 end)
 
 RegisterNUICallback('update', function(args, cb)
-    -- print('status', args.status)
+    print('status', args.status)
+    
+    if args.status == false then
+        SetNuiFocus(false, false)
+    end
+
+    cb('ok')
+end)
+
+RegisterNUICallback('updatemessage', function(args, cb)
+    print('Message', args.message)
     cb('ok')
 end)
--- ui/app.js
+++ ui/app.js
@@ -1,30 +1,87 @@
 const { createApp } = Vue
 
-  createApp({
-    data() {
-      return {
+createApp({
+  data() {
+    return {
+    	message: "Hello World",
+      tempmessage: "",
+      showing: false
+    }
+  },
+  mounted() {
+  	// setTimeout(()=>{
+    // 	this.showing = true
+    // }, 2000)
+
+    window.addEventListener('keypress', this.onKeypress)
+    // window.addEventListener('keyup', this.onKeypress)
+    // document.onKeypress = this.onKeypress
+
+    window.addEventListener('message', this.onMessage)
+  },
+  destroyed() {
+    window.removeEventListener('message')
+  },
+  computed: {
+  	changestyle() {
+    
+    	let color = ''
+      
+      if(this.message == 'Vorp Rules') {
+        color = 'red'
+      }
+    
+    	return {'color': color}
+    },
+    buttonstyle() {
+    	let showing = false
+  		if(this.message == 'Vorp Rules') {
+        showing = true
+      }
+    
+    	return {'difchangebutton': showing}
+    }
+  },
+  watch: {
+    showing(data) {
+      // Reset dat variables
+      this.message = "Hello World"
+      this.tempmessage = ""
+
+
+      // Send data to Nui/Lua
+      fetch(`https://${GetParentResourceName()}/update`, {
+        method: 'POST',
+        body: JSON.stringify({
+          status: data
+        })
+      })
+    }
+  },
+  methods: {
+    onKeypress(event) { 
+      if (event.key == 'q' || event.key == 'Q') {
+      	console.log('Q was pressed!')
       }
     },
-    mounted() {
-      window.addEventListener('message', this.onMessage);
+    changeMessage() {
+    	this.message = this.tempmessage
+      fetch(`https://${GetParentResourceName()}/updatemessage`, {
+        method: 'POST',
+        body: JSON.stringify({
+          message: this.message
+        })
+      })
+      /* this.$refs['cstyle'].style.color =  'red'*/
     },
-    destroyed() {
-      window.removeEventListener('message')
-    },
-    methods: {
-        onMessage(event) {
-          if (event.data.type === 'open') {
-          }
-        },
-        toggleUI(status) {
-          this.visible = status
-        
-          fetch(`https://${GetParentResourceName()}/update`, {
-            method: 'POST',
-            body: JSON.stringify({
-              status: this.visible
-            })
-          })
-        }
+    onMessage(event) {
+      if (event.data.type === 'open') {
+        this.showing = true
+      }
+      if (event.data.type === 'close') {
+        this.showing = false
+      }
     }
-  }).mount('#app')
+  }
+  
+}).mount('#app')
